c3e79e23d02112558cc85afa97d4968338834643
rafael <rafael.januario1212@gmail.com>
2024-08-19 22:41:23 -0300

    historia e o main do projeto

---
+++ index.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class index {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Jhon estava entediado e resolveu ir jogar no computador, mas ele não tinha um computador" +
+                "então resolveu comprar um saiu de casa e foi estava indo até a loja quando avistou um portal pixelado " +
+                "e por mera curiosidade entrou dentro dele e se viu dentro de uma guerra contra barbaros zumbis com o " +
+                "jimin do bts controlando eles e você avistou uma ESPADA");
+    }
+}
